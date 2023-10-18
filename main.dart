371fb550837ffb90a9fd60ad579e8aed78b6a33e
MD Ashikur Rahman <infoashikur887@gmail.com>
2023-10-18 20:25:29 +0600

    10 tutorial complate

---
+++ main.dart
@@ -0,0 +1,151 @@
+void main() {
+  // print("hello word");
+  // int value = 20 ;
+  // print(value);
+  // double values = 2;
+  // print(values);
+
+  //   String name = "Ashikur Rhoman";
+  //   print(name);
+  //   bool b = true;
+  //   print(b);
+  //   List my = ['one','two','three','four','five'];
+  //   print(my);
+
+  //   Map<String,dynamic> myMap ={
+  //   'name':'Ashikur Rhoman',
+  //   'age':25,
+  //   'age':30,
+  //   };
+  //   print(myMap);
+
+  // String connection="waiting";
+
+  // if(connection == 'connected'){
+  //   print('connected');
+  // }
+  // else if(connection == 'waiting'){
+  //   print('waiting');
+  // }else{
+  //   print('disconnected');
+  // }
+
+  // switch(connection)
+  // {
+  //   case 'connected':
+  //   print('connected');
+  //   break;
+  //   case 'waiting':
+  //   print('waiting');
+  //   break;
+  //   case 'disconnected':
+  //   print('disconnected');
+  // }
+
+  //string concatenation system
+
+  // String a ='we';
+  // String b ='are';
+  // String c ='learning';
+  // String d ='Dart';
+
+  // String e='we''are''learning''Dart';
+
+  // // 1 step
+  // print(a+b+c+d);
+
+  // // 2 step
+  // print('$a$b$c$d');
+
+  // // 3 step
+  // print(e);
+
+  // // 4 step
+  // List<String> f =['we''are''learning''Dart'];
+  // print(f.join());
+
+  // List<String> myList=['A','B','C','D'];
+  // print(myList);
+  // print(myList.length);
+  // myList.add('E');
+  // print(myList);
+  // myList.insert(0,'AA');
+  // print(myList);
+  // myList.replaceRange(0,1,['SS','DD','FF','GG']);
+  // print(myList);
+
+  // final gender = Gender.Male;
+  // switch(gender)
+  // {
+  //   case Gender.Male:
+  //   print('Gender is Male');
+  //   break;
+  //   case Gender.Female:
+  //   print('Gender is Female');
+  //   break;
+  //   case Gender.Unknown:
+  //   print('Gender is Unknown');
+  //   break;
+  //   default:
+  //   print('Nothing Matched');
+  // }
+
+  // enum Gender { Male,Female,Unknown}
+
+
+  // normal function
+  // normalfunction()
+  // {
+  //   print('Hello');
+  // }
+  // normalfunction();
+
+  //arrow function 
+
+  // arrowfunction()=>print('This is arrow Function');
+  // arrowfunction();
+
+  // parameterized function
+  // parameterized(num1,num2)
+  // {
+  //   print(num1+num2);
+  // }
+  // parameterized(20,30);
+  // parameterized(30,30);
+  // parameterized(40,30);
+  // parameterized(50,30);
+
+  //optional position parameter []
+
+  // optionalPosition(a,b,c,[d,e]){
+  //   print(a);
+  //   print(b);
+  //   print(c);
+  //   print(d);
+  //   print(e);
+  // }
+  // optionalPosition(20,50,30);
+
+  //optional named parameter {}
+
+  // optionalNamed(a,b,c,{d,e}){
+  //   print(a);
+  //   print(b);
+  //   print(c);
+  //   print(d);
+  //   print(e);
+  // }
+  // optionalNamed(20,50,30,d: 80,e:90);
+
+  // addToFunction(){
+  //   print(20+90);
+  // }
+
+  // mainFunction(addToFunction);
+
+}
+
+  // mainFunction(Function function)
+  // {
+  //   function();
+  // }
